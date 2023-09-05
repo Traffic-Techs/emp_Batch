97ed0f08d555c617d4324169fadea69bb6a74d95
SonMyungJi <linolousky@gmail.com>
2023-09-05 18:28:51 +0900

    git init : 데이터를 받아와서 전처리하고 계속 관리하는 목적

---
+++ src/main/java/com/example/empbatch/EmpBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.empbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpBatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmpBatchApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/empbatch/EmpBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpBatchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
