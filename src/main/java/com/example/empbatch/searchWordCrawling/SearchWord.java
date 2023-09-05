9a557c5800dd42e831c72f6a0f88dc67ef5c484d
SonMyungJi <linolousky@gmail.com>
2023-09-06 02:06:21 +0900

    feat : 네이터 데이터랩에서 일일 인기검색어 10위까지의 결과를 받아와서 데이터 추가

---
+++ src/main/java/com/example/empbatch/searchWordCrawling/SearchWord.java
@@ -0,0 +1,53 @@
+package com.example.empbatch.searchWordCrawling;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.annotation.PostConstruct;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SearchWord {
+
+  public static List<String> searchList = new ArrayList<>();
+
+  @PostConstruct
+  public void init() throws InterruptedException {
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--start-maximized");
+    options.addArguments("headless");
+
+    WebDriver driver = new ChromeDriver(options);
+
+    String url = "https://datalab.naver.com/home/sectionSearch.naver";
+    driver.get(url);
+
+    driver.findElement(By.xpath("//*[@id='content']/div[1]/div[3]/div[1]")).click();
+    driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div[3]/div[1]/ul/li[4]/a")).click();
+    Thread.sleep(10000);
+
+    List<WebElement> rankInnerElements = driver.findElements(By.cssSelector(".rank_inner.v2"));
+    if (!rankInnerElements.isEmpty()) {
+      WebElement lastElement = rankInnerElements.get(rankInnerElements.size() - 1);
+      List<WebElement> listAreaElements = lastElement.findElements(By.className("list_area"));
+
+      for (WebElement listAreaElement : listAreaElements) {
+        String word = listAreaElement.getText();
+        searchList.add(word);
+      }
+    }
+
+    if (driver != null) {
+      driver.quit();
+    }
+  }
+
+
+
+}
--- src/main/java/com/example/empbatch/util/UpdateDataRunner.java
+++ src/main/java/com/example/empbatch/util/UpdateDataRunner.java
@@ -1,20 +1,19 @@
 package com.example.empbatch.util;
 
-import com.example.empbatch.entity.Products;
-import com.example.empbatch.naver.dto.ItemDto;
 import com.example.empbatch.naver.service.NaverApiService;
 import com.example.empbatch.repository.ProductRepository;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import com.example.empbatch.searchWordCrawling.SearchWord;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public class TestDataRunner implements ApplicationRunner {
+@RequiredArgsConstructor
+public class UpdateDataRunner implements ApplicationRunner {
 
   @Autowired
   ProductRepository productRepository;
@@ -22,9 +21,14 @@ public class TestDataRunner implements ApplicationRunner {
   @Autowired
   NaverApiService naverApiService;
 
+  private final SearchWord searchWord;
+
   @Override
   public void run(ApplicationArguments args) {
-    createTestData("아이폰");
+    List<String> searchList = searchWord.searchList;
+    for (String searchItem : searchList) {
+      createTestData(searchItem);
+    }
   }
 
   private void createTestData(String searchWord) {
